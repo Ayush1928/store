2e6347fbf0f5fb6251a1b8544706bd4e6ca88d5e
Ayush1928 <96259539+Ayush1928@users.noreply.github.com>
2023-07-21 13:59:32 +0530

    Commit 12

--- Frontend/src/Pages/Home.js
+++ Frontend/src/Pages/Home.js
@@ -1,31 +1,30 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import Carousel from "../Components/Carousel";
 import Categories from "../Components/Categories";
 import Footer from "../Components/Footer";
 import Navbar from '../Components/Navbar';
 import Newletter from "../Components/Newsletter";
 import Products from "../Components/Products";
+import Register from "./Register"; 
 
 const Home = () => {
   const currentUser = useSelector((state) => state.user.currentUser);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!currentUser) {
-      navigate("/register");
-    }
-  }, [currentUser,navigate]);
 
   return (
     <div>
-      <Navbar/>
-      <Carousel/>
-      <Categories/>
-      <Products/>
-      <Newletter/>
-      <Footer/>
+      {currentUser ? ( 
+        <>
+          <Navbar />
+          <Carousel />
+          <Categories />
+          <Products />
+          <Newletter />
+          <Footer />
+        </>
+      ) : (
+        <Register />
+      )}
     </div>
   );
 }
