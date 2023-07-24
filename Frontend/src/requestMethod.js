85e2eb1d3975cc81ff3c2dca48388eef17025063
Ayush1928 <96259539+Ayush1928@users.noreply.github.com>
2023-07-24 13:24:47 +0530

    Commit 20

--- Frontend/src/requestMethod.js
+++ Frontend/src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://clothescornerbackend1-ayushbansal1928.b4a.run/api";
 let TOKEN = null;
 
 const persistedData = JSON.parse(localStorage.getItem("persist:root"));
